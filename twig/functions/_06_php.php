63c9df1bc84ef27c817b680e3801ad9f0337d3aa
Helmut Kaufmann <helmut.l.kaufmann@gmail.com>
2022-08-24 16:58:35 +0200

    modified:   twig/functions/_06_php.php

--- twig/functions/_06_php.php
+++ twig/functions/_06_php.php
@@ -3,24 +3,24 @@
 
 $functions += [
 
-		/**
-		* @deprecated as of v1.2: Use Twig standard filter "format_datetime"
-		*/
-		'strftime' => function ($time, $format = '%d.%m.%Y %H:%M:%S') {
+    /**
+    * @deprecated as of v1.2: Use Twig standard filter "format_datetime"
+    */
+    'strftime' => function ($time, $format = '%d.%m.%Y %H:%M:%S') {
         $timeObj = new Carbon($time);
         return strftime($format, $timeObj->getTimestamp());
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "upper"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "upper"
+     */
     , 'uppercase' => function ($string) {
         return mb_convert_case($string, MB_CASE_UPPER, "UTF-8");
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "lower"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "lower"
+     */
     , 'lowercase' => function ($string) {
         return mb_convert_case($string, MB_CASE_LOWER, "UTF-8");
     }
@@ -32,69 +32,69 @@ $functions += [
         return lcfirst($string);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "upper"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "upper"
+     */
     , 'uppercase' => function ($string) {
         return mb_convert_case($string, MB_CASE_UPPER, "UTF-8");
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "trimStart"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "trimStart"
+     */
     , 'ltrim' => function ($string, $charlist = " \t\n\r\0\x0B") {
         return ltrim($string, $charlist);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "trimEnd"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "trimEnd"
+     */
     , 'rtrim' => function ($string, $charlist = " \t\n\r\0\x0B") {
         return rtrim($string, $charlist);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "u.repeat"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "u.repeat"
+     */
     , 'str_repeat' => function ($string, $multiplier = 1) {
         return str_repeat($string, $multiplier);
     }
-		
+
     , 'plural' => function ($string, $count = 2) {
         return str_plural($string, $count);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "u.padBoth"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "u.padBoth"
+     */
     , 'strpad' => function ($string, $pad_length, $pad_string = ' ') {
         return str_pad($string, $pad_length, $pad_string, $pad_type = STR_PAD_BOTH);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "u.padStart"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "u.padStart"
+     */
     , 'leftpad' => function ($string, $pad_length, $pad_string = ' ') {
         return str_pad($string, $pad_length, $pad_string, $pad_type = STR_PAD_LEFT);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard filter "u.padEnd"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard filter "u.padEnd"
+     */
     , 'rightpad' => function ($string, $pad_length, $pad_string = ' ') {
         return str_pad($string, $pad_length, $pad_string, $pad_type = STR_PAD_RIGHT);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard function "reverse"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard function "reverse"
+     */
     , 'rtl' => function ($string) {
         return strrev($string);
     }
 
-		/**
- 		* @deprecated as of v1.2: Use Twig standard function "replace"
- 		*/
+    /**
+     * @deprecated as of v1.2: Use Twig standard function "replace"
+     */
     , 'str_replace' => function ($string, $search, $replace) {
         return str_replace($search, $replace, $string);
     }
