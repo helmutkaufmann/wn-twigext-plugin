f20096f2e1e85d0eb8741d6c2b35d6b85f4f99b2
Helmut Kaufmann <helmut.l.kaufmann@gmail.com>
2022-08-28 13:12:56 +0200

    new file:   twig/functions/_00_laravel.php 	renamed:    twig/functions/_01_cache_cookie_session_crypt.php -> twig/functions/_01_cache_cookie_session_crypt_deprecated.php

--- twig/functions/_01_cache_cookie_session_crypt_deprecated.php
+++ twig/functions/_01_cache_cookie_session_crypt_deprecated.php
@@ -1,52 +1,6 @@
 <?php
 
 //
-// Helper class allowing access to arbitray methods of arbitrary classes
-//
-class twigextStaticClass {
-
-	private $twigextCallerClass;
-
-	public function __construct($thisClass) {
-		$this->twigextCallerClass = $thisClass;
-	}
-
-	public function __call($method, $args) {
-			// Error handling to be added
-			return call_user_func_array(array($this->twigextCallerClass, $method), $args);
-	}
-}
-
-$functions += [
-
-	'cache' => function () {
-		  return new twigextStaticClass("Cache");
-	},
-	'storage' => function () {
-		  return new twigextStaticClass("Storage");
-	},
-	'crypt' => function () {
-		  return new twigextStaticClass("Crypt");
-	},
-	'cookie' => function () {
-		  return new twigextStaticClass("Cookie");
-	},
-	'hash' => function () {
-		  return new twigextStaticClass("Hash");
-	},
-	'session' => function () {
-		  return new twigextStaticClass("Session");
-	},
-	'collection'=> function ($name) {
-		  return new twigextStaticClass("Collection");
-	},
-	'accesObject'=> function ($name) {
-		  return new twigextStaticClass($name);
-	},
-
-];
-
-//
 // The following functions will be deprecated
 //
 $functions += [
